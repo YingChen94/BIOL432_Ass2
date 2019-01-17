1f7db6fc8233db3b4685838f02f687b47dd44c0a
Ying Chen <ying@Yings-MacBook-Pro.local>
2019-01-16 22:25:41 -0500

    R script for ass 2

--- Ass 2.R
+++ Ass 2.R
@@ -10,3 +10,29 @@
 ## If there is a column called ‘Nutrients’, replace each string with its first letter – for example ‘high’ becomes ‘h’, ‘moderate’ becomes ‘m’, etc. (You don’t know what all the options might be)
 ## Replace all periods . with commas , in the ‘TotalG’ column
 
+# my scripts 
+# load datafile 
+mydata <- read.csv("InData.csv")
+str(mydata)
+
+library(dplyr)
+# Remove rows with ‘Total’ biomass < 60
+mydata2 <- mydata %>% filter(Total >= 60)
+head(mydata2)
+# Reorder the columns so that they are in the order: ‘Total’, ‘Taxon’, ‘Senario’, ‘Nutrients’, and remove the other columns
+mydata3 <- mydata2 %>% select(Total, Taxon, Scenario, Nutrients)
+# Make a new column TotalG, which converts the ‘Total’ column from mg to grams AND replace Total with TotalG.
+mydata3$TotalG <- mydata3$Total/1000
+mydata4 <- mydata3 %>% select(TotalG, Taxon, Scenario, Nutrients) 
+#If there is a column called ‘Nutrients’, replace each string with its first letter – for example ‘high’ becomes ‘h’, ‘moderate’ becomes ‘m’, etc. (You don’t know what all the options might be)
+if ("Nutrients" %in% names(mydata4) == T)  {
+  print("Yes, there is a column called 'Nutrients'")
+  gsub("(\\w)\\w+", "\\1", mydata4$Nutrients)
+} else {
+  print("Sorry. There is no column called 'Nutrients'")
+}
+# Replace all periods . with commas , in the ‘TotalG’ column
+gsub("\\D", ",", mydata4$TotalG)
+
+
+
